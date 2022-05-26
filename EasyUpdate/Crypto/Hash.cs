df4faca328eef032e4a2559e220e24ab1ca1a8d3
WinExp <1450422389@qq.com>
2022-05-26 19:56:11 +0800

    Crypto: Add validation hash function.

---
+++ EasyUpdate/Crypto/Hash.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyUpdate.Crypto
+{
+    internal static class Hash
+    {
+        internal static string ComputeFileHash(string filePath, string hashName)
+        {
+            using (FileStream file = new FileStream(filePath, FileMode.Open))
+            {
+                using (HashAlgorithm hash = HashAlgorithm.Create(hashName))
+                {
+                    byte[] hashBuffer = hash.ComputeHash(file);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    foreach (var hashByte in hashBuffer)
+                    {
+                        stringBuilder.Append(hashByte.ToString("x2"));
+                    }
+                    return stringBuilder.ToString();
+                }
+            }
+        }
+    }
+}
